5280ac6c390c9154445bc1893c04baa1c0a52413
Guillermo Sangerman <sangermanmptn8@gmail.com>
2025-02-05 11:49:16 -0600

    se agregaron animaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,30 +24,34 @@ function Home() {
         setImagenCargada(true);
     }
     return (
-        <main id="canvas" className="fondo w-full min-h-screen bg-no-repeat bg-cover flex items-center justify-center bg-center ">
+        <main 
+            id="canvas" 
+            className="fondo w-full min-h-screen bg-no-repeat bg-cover flex items-center justify-center bg-center">
             {
                 !valueSi ? (
-                    <div className="p-5 mt-5">
-                        <h1 className="text-white font-bold text-5xl text-center font-serif">¿Quieres ser mi San Valentin?</h1>
+                    <div className="p-5 mt-5 ">
+                        <h1 className="text-red-600  font-bold text-5xl text-center font-serif move-up">
+                            ¿Quieres Ser Mi San Valentin?                           
+                            </h1>
                         <img src={Object.keys(randomValor).length === 0 ?
                             "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif" : randomValor.img}
-                            alt="San Valentin" className="mx-auto" width={400} height={400} />
+                            alt="San Valentin" className="mx-auto visible-animation" width={400} height={400} />
                         <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-5 items-center">
                             <button onClick={() => {
                                 setValueSi(true)
 
                                 jsConfetti.addConfetti({
-                                    emojis: ['😍', '🥰', '❤️', '😘'],
+                                    emojis: ['😍', '🥰', '❤️', '🌻'],
                                     emojiSize: 70,
                                     confettiNumber: 80,
                                 })
 
-                            }} className={`bg-green-500 text-white font-bold p-2 rounded-md text-xl h-${agrandar}`}
+                            }} className={`bg-gradient-to-r from-red-700 via-red-500 to-red-300 text-white font-bold p-2 rounded-md text-xl h-${agrandar} move-left`}
                                 style={{ height: agrandar }}>
                                 Si
                             </button>
                             <button
-                                className="bg-red-500 text-white font-bold p-2 rounded-md text-xl"
+                                className="bg-gradient-to-r from-red-300 via-red-500 to-red-700 text-white font-bold p-2 rounded-md text-xl move-rigth"
                                 onClick={randomResponse}
                                 disabled={imagenCargada} // Deshabilita el botón si la imagen no se ha cargado
                             >
@@ -61,9 +65,9 @@ function Home() {
                     </div>
                 ) : (
                     <div className="flex justify-center items-center flex-col space-y-10 mt-5">
-                        <h1 className="text-2xl md:text-4xl text-white font-bold font-serif">Sabia que dirias que si ❤️!</h1>
+                        <h1 className="text-2xl md:text-4xl text-red-600 font-bold font-serif rotation-spin">Sabia que dirias que si ❤️!</h1>
                         <img src="https://i.pinimg.com/originals/9b/dc/c6/9bdcc6206c1d36a37149d31108c6bb41.gif"
-                            alt="gif abrazados" className="mx-auto" />
+                            alt="gif abrazados" className="mx-auto rotation-y" />
                         <span hidden>{document.title = 'Sabia que dirias que si ❤️!'}</span>
                     </div>
                 )
